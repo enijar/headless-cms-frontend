63ee15b3926881e305a27e11240893348baa2cfe
Enijar <TheEnijar@gmail.com>
2019-10-19 16:50:32 +0100

    Updated authentication tests

--- test/authentication.js
+++ test/authentication.js
@@ -4,7 +4,8 @@ import { describe, it } from "mocha";
 import fetchMock from "fetch-mock";
 import services from "../src/app/services";
 
-const VALID_USER_RESPONSE = {user: {}};
+const VALID_RESPONSE = {user: {}};
+const INVALID_RESPONSE = null;
 
 describe('authentication', () => {
   it('return false when JWT is not set', async () => {
@@ -12,11 +13,18 @@ describe('authentication', () => {
     expect(authenticated).to.equal(false);
   });
 
-  it('return true when JWT is valid', async () => {
-    fetchMock.mock('/api/user', 200, {response: VALID_USER_RESPONSE});
+  it('return user object from API when JWT is valid', async () => {
+    fetchMock.mock('/api/user', 200, {response: VALID_RESPONSE});
     const authenticated = await services.authenticate('/', 'valid-jwt');
     fetchMock.restore();
-    expect(authenticated).to.include.keys('user');
+    expect(authenticated).to.eql(VALID_RESPONSE);
+  });
+
+  it('return false from API when JWT is valid', async () => {
+    fetchMock.mock('/api/user', 401, {response: INVALID_RESPONSE});
+    const authenticated = await services.authenticate('/', 'invalid-jwt');
+    fetchMock.restore();
+    expect(authenticated).to.equal(INVALID_RESPONSE);
   });
 
   fetchMock.reset();
